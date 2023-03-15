983def60a7c796c160d49d60aa1b484e39adc005
Abhishek <abhishek.pandey@ucertify.com>
2023-03-15 08:30:03 +0530

    phpindexupd

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 /**
- * This file contains code that adds two numbers and displays the result.
- *
- * @author John Doe
- * @copyright Copyright (c) 2023
- */
+    *  File-name         : pe-gold3/chatbot/index.php
+    *  Created at        : 30-Jan-2023
+    *
+    *  @author           : Md Amzad <mohd.amzad@ucertify.com>
+    *  @version          : 1.0
+    *  @package          : chatbot
+    *  Last updated date : 10-Feb-2023
+    *  @detail           : For fetching the previous chat data showing the chat UI to the user when mentor button click.
+    */
  
 $x = 15;
 $y = 30;
 $z = $x + $y;
 
 echo "Sum: " . $z;
+
+
