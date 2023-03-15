bd9237cc694a252f4ba7ad99846edb95316f5983
Abhishek <abhishek.pandey@ucertify.com>
2023-03-15 13:03:00 +0530

    d

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
-/**
-    *  File-name         : pe-gold3/chatbot/index.php
-    *  Created at        : 30-Jan-2023
-    *
-    *    @author   :  Md Amzad <mohd.amzad@ucertify.com>
-        *    @version : 1.0
-    *  @package : chatbot
-    *  Last updated date : 10-Feb-2023
-    *  @detail  : For fetching the previous chat data showing the chat UI to the user when mentor button click.
-    */
- 
-$x = 15;
-$y = 30;
-$z = $x + $y;
 
-echo "Sum: " . $z;
+function say_hello($name)
+{
+    echo "Hello, " . $name . "!";
+}
 
+$name = "John";
+say_hello($name);
 
+?>
