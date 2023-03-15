4d8093b57df8500f39a1bae659d18040d4b29876
Abhishek <abhishek.pandey@ucertify.com>
2023-03-15 08:41:22 +0530

    commitindex1

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     *  File-name         : pe-gold3/chatbot/index.php
     *  Created at        : 30-Jan-2023
     *
-    *  @author  :  Md Amzad <mohd.amzad@ucertify.com>
-    *  @version : 1.0
+    *    @author   :  Md Amzad <mohd.amzad@ucertify.com>
+        *    @version : 1.0
     *  @package : chatbot
     *  Last updated date : 10-Feb-2023
     *  @detail  : For fetching the previous chat data showing the chat UI to the user when mentor button click.
